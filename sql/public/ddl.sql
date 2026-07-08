b3add22b1b1cff8393fc6313a694b930db2265d7
ChanX <107697626+ChanX-AI@users.noreply.github.com>
2026-07-08 22:35:32 +0530

    Update ddl.sql

--- sql/public/ddl.sql
+++ sql/public/ddl.sql
@@ -1,1 +1,55 @@
-/* */
+/*
+================================================================
+	Public Layer
+================================================================
+	-> All the core food tables are created
+	
+	** Warnings **
+		The drop command is used before creating a table
+*/
+
+-- food data types
+DROP TABLE IF EXISTS public.food_data_types;
+
+CREATE TABLE public.food_data_types (
+	id integer PRIMARY KEY,
+	name text
+);
+
+-- food categories
+DROP TABLE IF EXISTS public.food_categories;
+
+CREATE TABLE public.food_categories (
+	id integer PRIMARY KEY,
+	name text
+);
+
+-- foods
+DROP TABLE IF EXISTS public.foods;
+
+CREATE TABLE public.foods (
+	id integer PRIMARY KEY,
+	name text,
+	category_id integer,
+	data_type_id integer
+);
+
+-- core nutrients
+DROP TABLE IF EXISTS public.core_nutrients;
+
+CREATE TABLE public.core_nutrients (
+	id integer PRIMARY KEY,
+	name text,
+	units text
+);
+
+DROP TABLE IF EXISTS public.food_nutrients;
+
+CREATE TABLE public.food_nutrients (
+	food_id integer REFERENCES public.foods (id),
+	nutrient_id integer REFERENCES public.core_nutrients (id),
+	amount integer
+);
+
+
+
