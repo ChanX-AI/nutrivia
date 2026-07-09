8aed59d34095c07c4448b00d53a87c944c86294e
ChanX <107697626+ChanX-AI@users.noreply.github.com>
2026-07-09 08:21:04 +0530

    Update ddl.sql

--- sql/stage/sr_legacy/ddl.sql
+++ sql/stage/sr_legacy/ddl.sql
@@ -4,19 +4,13 @@
 ================================================================
 	-> All the core sr legacy food tables are created and loaded
 	
-	** Warnings **
-		The drop command is used before creating a table
+	** WARNINGS **
+		The DROP command is used before creating a table
 	
 	Note: 
 		* All tables are loaded accordingly with the USDA food data after creation
 
 */
-
-
-
--- =====================================
--- sr legacy food tables
--- ======================================
 
 -- food
 DROP TABLE IF EXISTS sr_legacy_stg.food;
@@ -25,7 +19,7 @@ CREATE TABLE sr_legacy_stg.food (
     fdc_id integer PRIMARY KEY,
     data_type text,
     description text,
-    food_category_id integer REFERENCES public.food_category (id),
+    food_category_id integer REFERENCES common.food_category (id),
     publication_date text
 );
 
@@ -43,7 +37,7 @@ DROP TABLE IF EXISTS sr_legacy_stg.food_nutrient;
 CREATE TABLE sr_legacy_stg.food_nutrient (
     id integer PRIMARY KEY,
     fdc_id integer REFERENCES sr_legacy_stg.food (fdc_id),
-    nutrient_id integer REFERENCES public.nutrient (id),
+    nutrient_id integer REFERENCES common.nutrient (id),
     amount numeric(8,2),
     data_point text,
     derivation_id text,
