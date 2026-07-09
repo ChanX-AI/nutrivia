42bdc5f7ec776693a4e2086cc69357c2f9d9d9dd
ChanX <107697626+ChanX-AI@users.noreply.github.com>
2026-07-09 08:13:35 +0530

    Update ddl.sql

--- sql/stage/common/ddl.sql
+++ sql/stage/common/ddl.sql
@@ -1,1 +1,30 @@
-/**/
+/*
+  =========================================
+    Stage common
+  =========================================
+    -> tables that are common to stage foundation and stage sr legacy are created
+
+    ** WARNINGS **
+        * DROP command is used before creating tables
+*/
+
+-- nutrient
+DROP TABLE IF EXISTS common.nutrient;
+
+CREATE TABLE common.nutrient (
+    id integer PRIMARY KEY,
+    name text,
+    unit_name text,
+    nutrient_nbr text,
+    rank text
+);
+
+-- food category
+DROP TABLE IF EXISTS common.food_category;
+
+CREATE TABLE common.food_category (
+  id integer PRIMARY KEY,
+  code integer,
+  description text
+);
+
