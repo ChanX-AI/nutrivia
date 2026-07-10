68693cbf67c835efce0d9d5646d0e177042b8db3
ChanX <107697626+ChanX-AI@users.noreply.github.com>
2026-07-10 12:52:16 +0530

    Update load.sql

--- src/sql/public/load.sql
+++ src/sql/public/load.sql
@@ -57,7 +57,7 @@ SELECT
      name,
      TRIM(unit_name) AS units
 FROM common.nutrient
-WHERE id IN (1008, 1003, 1004, 1079, 2000, 1253, 1051, 1005, 1079);
+WHERE id IN (1008, 1003, 1004, 1079, 2000, 1253, 1051, 1005);
 
 -- food nutrients
 TRUNCATE TABLE public.food_nutrients;
